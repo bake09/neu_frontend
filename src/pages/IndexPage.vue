9a0c5e4ab4ebd0d2cebc1356fe37fca7ce52400e
Basar baok. Ökke <baok@weller.lan>
2024-04-22 10:00:06 +0200

    Edited BootFile axiosConfig and IndexPage for Testing

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -1,8 +1,24 @@
 <template>
-  <q-page class="flex flex-center">
+  <q-page class="q-pa-md">
     <h4>Index Page</h4>
+    <pre>{{ tasks }}</pre>
   </q-page>
 </template>
 
 <script setup>
+import { onMounted, ref } from "vue";
+import { api } from "boot/axios";
+
+const tasks = ref([])
+
+onMounted(() => {
+  api.get('/api/task').then(res => {
+    console.log(res)
+    res.data.forEach(item => {
+      tasks.value.push(item)
+    })
+  }).catch(err => {
+    console.log(err)
+  })
+})
 </script>
