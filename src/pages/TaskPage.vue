296bd68675543a6b8f7331f75ebc240ebfd36aea
Basar baok. Ökke <baok@weller.lan>
2024-04-23 08:03:36 +0200

    latest Frontend commit

--- src/pages/TaskPage.vue
+++ src/pages/TaskPage.vue
@@ -1,9 +1,34 @@
 <template>
   <q-page class="q-pa-md">
     <div class="text-h6">Tasks Page</div>
-    <q-scroll-area style="height: 200px; max-width: 100%;" 
+    <div class="row q-mb-md column">
+      <q-input
+        rounded
+        outlined
+        dense
+        bottom-slots
+        v-model="taskStore.newTask"
+        label="Label"
+        :dense="dense">
+        <template v-slot:hint>
+          Field hint
+        </template>
+  
+        <template v-slot:after>
+          <q-btn
+            round
+            flat
+            icon="send"
+            @click.prevent="taskStore.addTask" />
+        </template>
+      </q-input>
+    </div>
+
+    <q-scroll-area
+      style="height: 300px; max-width: 100%;"
       :thumb-style="settingsStore.thumbStyle"
-      :bar-style="settingsStore.barStyle">
+      :bar-style="settingsStore.barStyle"
+      ref="tasksBox">
       <q-list bordered separator v-if="taskStore.tasks.length">
         <q-item clickable v-ripple v-for="task in taskStore.tasks" :key="task.id">
           <q-item-section avatar>
@@ -13,21 +38,54 @@
           </q-item-section>
 
           <q-item-section>{{ task.content }}</q-item-section>
+          <q-item-section avatar>
+            <q-btn flat round color="red" icon="delete" @click.stop="taskStore.deleteTask(task)"/>
+          </q-item-section>
         </q-item>
       </q-list>
-    </q-scroll-area>
-    
-    <q-inner-loading
+      <q-inner-loading
         :showing="taskStore.tasksIsLoading"
         label="loading..."
         label-class="primary"
         label-style="font-size: 1.1em"
       />
+    </q-scroll-area>
+    
   </q-page>
 </template>
 
 <script setup>
-import { onMounted, ref } from "vue";
+import { onMounted, ref, nextTick } from "vue";
+import { useMeta } from 'quasar'
+
+const metaData = {
+  // sets document title
+  title: 'Tasks Page',
+  // optional; sets final title as "Index Page - My Website", useful for multiple level meta
+  titleTemplate: title => `${title} - Frontend App`,
+
+  // meta tags
+  meta: {
+    description: { name: 'description', content: 'Page 1' },
+    keywords: { name: 'keywords', content: 'Quasar website' },
+    // note: for Open Graph type metadata you will need to use SSR, to ensure page is rendered by the server
+    ogTitle:  {
+      property: 'og:title',
+      // optional; similar to titleTemplate, but allows templating with other meta properties
+      template (ogTitle) {
+        return `${ogTitle} - Frontend App`
+      }
+    }
+  },
+}
+
+useMeta(metaData)
+
+
+
+
+
+
 
 import { useTaskStore } from "stores/task-store";
 const taskStore = useTaskStore()
@@ -35,7 +93,17 @@ const taskStore = useTaskStore()
 import { useSettingsStore } from "stores/settings-store";
 const settingsStore = useSettingsStore()
 
+const tasksBox = ref(null)
+
+const position = ref(300)
+const animateScroll = () => {
+  tasksBox.value.setScrollPosition('vertical', position.value, 300)
+}
+
 onMounted(() => {
   taskStore.allTask()
+  console.log(tasksBox.value)
+  nextTick()
+  animateScroll()
 })
 </script>
--- src/stores/task-store.js
+++ src/stores/task-store.js
@@ -4,7 +4,8 @@ import { api } from "boot/axios";
 export const useTaskStore = defineStore('task', {
   state: () => ({
     tasks: [],
-    tasksIsLoading: false
+    tasksIsLoading: false,
+    newTask: ''
   }),
   getters: {
     doubleCount (state) {
@@ -24,6 +25,35 @@ export const useTaskStore = defineStore('task', {
       }).catch(err => {
         console.log(err)
         this.tasksIsLoading = false
+      })
+    },
+    addTask() {
+      this.tasksIsLoading = true
+      api.post('/api/task', { content: this.newTask}).then(res => {
+        this.tasksIsLoading = false
+        this.newTask = ''
+        this.tasks.push(res.data.task)
+      }).catch(err => {
+        console.log(err)
+        this.tasksIsLoading = false
+        this.newTask = ''
+      })
+    },
+    updateTask(task) {
+      console.log(task)
+    },
+    deleteTask(task) {
+      this.tasksIsLoading = true
+      api.delete(`/api/task/${task.id}`).then(res => {
+        this.tasksIsLoading = false
+        this.tasks.forEach((item, index) => {
+          if (item.id === task.id) {
+            this.tasks.splice(index, 1);
+          }
+        });
+      }).catch(err => {
+        this.tasksIsLoading = false
+        console.log(err)
       })
     }
   }
